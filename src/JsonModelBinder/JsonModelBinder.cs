565eb8fdc113502c1ef2df06a32eefb3dadfe070
Bruno Zell <bruno.zzell@gmail.com>
2019-10-04 05:56:00 +0200

    multi targeting and correct package versions

--- src/JsonModelBinder/JsonModelBinder.cs
+++ src/JsonModelBinder/JsonModelBinder.cs
@@ -1,24 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Newtonsoft.Json;
 using System;
 using System.Threading.Tasks;
+
+#if NETCOREAPP1_0 || NETCOREAPP2_0
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+#endif
 
 namespace BrunoZell.ModelBinding
 {
     public class JsonModelBinder : IModelBinder
     {
+#if NETCOREAPP1_0 || NETCOREAPP2_0
         private readonly MvcJsonOptions _options;
 
         public JsonModelBinder(IOptions<MvcJsonOptions> options) =>
             _options = options.Value;
+#endif
 
         public Task BindModelAsync(ModelBindingContext bindingContext)
         {
-            if (bindingContext == null) {
+            if (bindingContext == null)
                 throw new ArgumentNullException(nameof(bindingContext));
-            }
 
             // Test if a value is received
             var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
@@ -29,23 +33,19 @@ namespace BrunoZell.ModelBinding
                 string serialized = valueProviderResult.FirstValue;
 
                 // Use custom json options defined in startup if available
+#if NETCOREAPP1_0 || NETCOREAPP2_0
                 object deserialized = _options?.SerializerSettings == null ?
                     JsonConvert.DeserializeObject(serialized, bindingContext.ModelType) :
                     JsonConvert.DeserializeObject(serialized, bindingContext.ModelType, _options.SerializerSettings);
+#else
+                object deserialized = JsonConvert.DeserializeObject(serialized, bindingContext.ModelType);
+#endif
 
                 // Set successful binding result
                 bindingContext.Result = ModelBindingResult.Success(deserialized);
-#if NET451
-                return Task.FromResult(0);
-#else
-                return Task.CompletedTask;
-#endif
             }
-#if NET451
-            return Task.FromResult(0);
-#else
+
             return Task.CompletedTask;
-#endif
         }
     }
 }
