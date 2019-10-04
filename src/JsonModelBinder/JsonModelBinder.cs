d23b67f3ba566d891f2bbd1a4f345bb9ed5367a3
Bruno Zell <bruno.zzell@gmail.com>
2019-10-04 06:04:59 +0200

    input validation

--- src/JsonModelBinder/JsonModelBinder.cs
+++ src/JsonModelBinder/JsonModelBinder.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Newtonsoft.Json;
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 #if NETCOREAPP1_0 || NETCOREAPP2_0
@@ -40,6 +44,28 @@ namespace BrunoZell.ModelBinding
 #else
                 object deserialized = JsonConvert.DeserializeObject(serialized, bindingContext.ModelType);
 #endif
+
+                // DataAnnotation Validation. Validate Properties and Fields.
+                var validationResultProps = from property in TypeDescriptor.GetProperties(deserialized).Cast<PropertyDescriptor>()
+                                            from attribute in property.Attributes.OfType<ValidationAttribute>()
+                                            where !attribute.IsValid(property.GetValue(deserialized))
+                                            select new {
+                                                Property = property.Name,
+                                                ErrorMessage = attribute.FormatErrorMessage(String.Empty)
+                                            };
+
+                var validationResultFields = from field in TypeDescriptor.GetReflectionType(deserialized).GetFields().Cast<FieldInfo>()
+                                             from attribute in field.GetCustomAttributes<ValidationAttribute>()
+                                             where !attribute.IsValid(field.GetValue(deserialized))
+                                             select new {
+                                                 Property = field.Name,
+                                                 ErrorMessage = attribute.FormatErrorMessage(String.Empty)
+                                             };
+
+                // Add the ValidationResult's to the ModelState
+                var errors = validationResultFields.Concat(validationResultFields);
+                foreach (var validationResultItem in errors)
+                    bindingContext.ModelState.AddModelError(validationResultItem.Property, validationResultItem.ErrorMessage);
 
                 // Set successful binding result
                 bindingContext.Result = ModelBindingResult.Success(deserialized);
