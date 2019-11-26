e065011a71aab08612e26aa19d9aac2fce6fff7c
Bruno Zell <bruno.zzell@gmail.com>
2019-11-26 07:09:58 +0100

    cleanup and better comments

--- src/JsonModelBinder/JsonModelBinder.cs
+++ src/JsonModelBinder/JsonModelBinder.cs
@@ -29,47 +29,48 @@ namespace BrunoZell.ModelBinding
 
         public Task BindModelAsync(ModelBindingContext bindingContext)
         {
-            if (bindingContext == null)
+            if (bindingContext is null)
                 throw new ArgumentNullException(nameof(bindingContext));
 
             // Test if a value is received
             var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
-            if (valueProviderResult != ValueProviderResult.None) {
-                bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
+            if (valueProviderResult == ValueProviderResult.None)
+                return Task.CompletedTask;
 
-                // Deserialize from string
-                string serialized = valueProviderResult.FirstValue;
+            bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
 
-                // Use custom json options defined in startup if available
-                object deserialized = _options?.SerializerSettings == null ?
-                    JsonConvert.DeserializeObject(serialized, bindingContext.ModelType) :
-                    JsonConvert.DeserializeObject(serialized, bindingContext.ModelType, _options.SerializerSettings);
+            // Get the json serialized value as string
+            string serialized = valueProviderResult.FirstValue;
 
-                // DataAnnotation Validation. Validate Properties and Fields.
-                var validationResultProps = from property in TypeDescriptor.GetProperties(deserialized).Cast<PropertyDescriptor>()
-                                            from attribute in property.Attributes.OfType<ValidationAttribute>()
-                                            where !attribute.IsValid(property.GetValue(deserialized))
-                                            select new {
-                                                Property = property.Name,
-                                                ErrorMessage = attribute.FormatErrorMessage(String.Empty)
-                                            };
+            // Deserialize json string using custom json options defined in startup, if available
+            object deserialized = _options?.SerializerSettings is null ?
+                JsonConvert.DeserializeObject(serialized, bindingContext.ModelType) :
+                JsonConvert.DeserializeObject(serialized, bindingContext.ModelType, _options.SerializerSettings);
 
-                var validationResultFields = from field in TypeDescriptor.GetReflectionType(deserialized).GetFields().Cast<FieldInfo>()
-                                             from attribute in field.GetCustomAttributes<ValidationAttribute>()
-                                             where !attribute.IsValid(field.GetValue(deserialized))
-                                             select new {
-                                                 Property = field.Name,
-                                                 ErrorMessage = attribute.FormatErrorMessage(String.Empty)
-                                             };
+            // Run data annotation validation to validate properties and fields on deserialized model
+            var validationResultProps = from property in TypeDescriptor.GetProperties(deserialized).Cast<PropertyDescriptor>()
+                                        from attribute in property.Attributes.OfType<ValidationAttribute>()
+                                        where !attribute.IsValid(property.GetValue(deserialized))
+                                        select new {
+                                            Member = property.Name,
+                                            ErrorMessage = attribute.FormatErrorMessage(String.Empty)
+                                        };
 
-                // Add the ValidationResult's to the ModelState
-                var errors = validationResultFields.Concat(validationResultFields);
-                foreach (var validationResultItem in errors)
-                    bindingContext.ModelState.AddModelError(validationResultItem.Property, validationResultItem.ErrorMessage);
+            var validationResultFields = from field in TypeDescriptor.GetReflectionType(deserialized).GetFields().Cast<FieldInfo>()
+                                         from attribute in field.GetCustomAttributes<ValidationAttribute>()
+                                         where !attribute.IsValid(field.GetValue(deserialized))
+                                         select new {
+                                             Member = field.Name,
+                                             ErrorMessage = attribute.FormatErrorMessage(String.Empty)
+                                         };
 
-                // Set successful binding result
-                bindingContext.Result = ModelBindingResult.Success(deserialized);
-            }
+            // Add the validation results to the model state
+            var errors = validationResultFields.Concat(validationResultFields);
+            foreach (var validationResultItem in errors)
+                bindingContext.ModelState.AddModelError(validationResultItem.Member, validationResultItem.ErrorMessage);
+
+            // Set successful binding result
+            bindingContext.Result = ModelBindingResult.Success(deserialized);
 
             return Task.CompletedTask;
         }
